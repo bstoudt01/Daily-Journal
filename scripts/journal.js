00f27d1b3dc2206c4d157e58c82cfdc096926d1a
Brett Stoudt <Bstoudt01@gmail.com>
2020-06-02 16:38:36 -0500

    added some js.. not sure if its to simple

---
+++ scripts/journal.js
@@ -0,0 +1,27 @@
+var journalEntries = [];
+
+const journalEntry1 = {
+    date: "6.1.20",
+    title: "jscript",
+    entry: "we jumped into javascript today and Steve walked us through creating modular javascrip components",
+    mood: "potato"
+};
+journalEntries.push(journalEntry1);
+
+const journalEntry2 = {
+    date: "4.1.20",
+    title: "more and more jscript",
+    entry: "we jumped into javascript today and Steve walked us through creating modular javascrip components",
+    mood: "haeady"
+};
+    journalEntries.push(journalEntry2);
+
+const journalEntry3 = {
+    date: "4.1.20",
+    title: "more and more jscript",
+    entry: "we jumped into javascript today and Steve walked us through creating modular javascrip components",
+    mood: "haeady"
+};
+    journalEntries.push(journalEntry3);
+        
+console.log(journalEntries)
---
+++ scripts/journalData.js
@@ -0,0 +1,13 @@
+const journalEntry = {
+    date: "6.1.20",
+    title: "jscript",
+    entry: "we jumped into javascript today and Steve walked us through creating modular javascrip components",
+    mood: "potato"
+    },
+const journalEntry = {
+        date: "1.1.20",
+        title: "hello",
+        entry: "day2",
+        mood: "heady"
+        };
+    
