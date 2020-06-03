264a77e08de2c362fba69bf3bdd93f6a65a350f0
Brett Stoudt <Bstoudt01@gmail.com>
2020-06-03 16:13:19 -0500

    created functions to convert data to html string and display on my webapp under my entry form

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,27 +1,48 @@
-var journalEntries = [];
+var journalEntries = [
+    {
+        date: "6.1.20",
+        title: "jscript",
+        entry: "we jumped into javascript today and Steve walked us through creating modular javascrip components",
+        mood: "potato"
+    },
+     
+    {
+        date: "6.2.20",
+        title: "more and more jscript",
+        entry: "we jumped into javascript today and Steve walked us through creating modular javascrip components",
+        mood: "heady"
+    },
+    
+    {
+        date: "6.3.20",
+        title: "functions in functions",
+        entry: "we jumped into javascript today and Steve walked us through creating modular javascrip components",
+        mood: "heady"
+    }
+];
 
-const journalEntry1 = {
-    date: "6.1.20",
-    title: "jscript",
-    entry: "we jumped into javascript today and Steve walked us through creating modular javascrip components",
-    mood: "potato"
-};
-journalEntries.push(journalEntry1);
 
-const journalEntry2 = {
-    date: "4.1.20",
-    title: "more and more jscript",
-    entry: "we jumped into javascript today and Steve walked us through creating modular javascrip components",
-    mood: "haeady"
-};
-    journalEntries.push(journalEntry2);
-
-const journalEntry3 = {
-    date: "4.1.20",
-    title: "more and more jscript",
-    entry: "we jumped into javascript today and Steve walked us through creating modular javascrip components",
-    mood: "haeady"
-};
-    journalEntries.push(journalEntry3);
+const journalEntryConverter = (journalEntry) => {
+ const journalHTMLRepresentation = `<div class="journalEntryContainer">
+ <section class = "entryLog__date">Date: ${journalEntry.date}</section>
+ <section class = "entryLog__title">Concepts Covered: ${journalEntry.title}</section>
+ <section class = "entryLog__entry">Journal Entry: ${journalEntry.entry}</section>
+ <section class = "entryLog__mood>Mood: ${journalEntry.mood}</section>
+</div>
+</article> `
+    // Create your own HTML structure for a journal entry
+    return journalHTMLRepresentation
+}
         
-console.log(journalEntries)
+const showJournalEntries = () => {
+    const journalObjectsArray = journalEntries
+
+    for (const journalObject of journalObjectsArray){
+        const journalHTMLRepresentation = journalEntryConverter(journalObject)
+        const journalArticleElement = document.querySelector(".entryLog")
+        journalArticleElement.innerHTML += journalHTMLRepresentation
+    }
+
+}
+
+showJournalEntries()
