d6bae1bfac89678601510df3c75be892760a6000
Brett Stoudt <Bstoudt01@gmail.com>
2020-06-22 21:04:58 -0500

    placed radio button on html and started javascript listener w. forEach and .filter

--- scripts/journal.js
+++ scripts/journal.js
@@ -24,7 +24,7 @@ registerListeners () {
     journalLogContainer.addEventListener("click", event => {
             //DELETE ENTRY BUTTON
             if (event.target.id.startsWith("deleteEntry--")) {
-                // Extract entry id from the button's id attribute
+                // Extract UNIQUE entry id from the button's id attribute
                 const entryToDelete = event.target.id.split("--")[1]
 
                 // Invoke the delete method, then get all entries and render them
@@ -35,7 +35,7 @@ registerListeners () {
 
             //EDIT ENTRY BUTTON
             else if (event.target.id.startsWith("editEntry--")) {
-                // Extract entry id from the button's id attribute
+                // Extract UNIQUE entry id from the button's id attribute, id created in html component
                 //.split turns a strign into arrays at the chosen spot"--", and then you must specify which part of the array you want to return[1]
                 const entryToEdit = event.target.id.split("--")[1]
 
@@ -74,11 +74,15 @@ saveJournalEditsButton.addEventListener("click", event => {
 		const journalDateInput = document.querySelector("#journalDate").value;
 		const journalTitleInput = document.querySelector("#journalTitle").value;
 		const journalEntryInput = document.querySelector("#journalEntry").value;
-		const journalMoodInput = document.querySelector("#journalMood").value;
-		//(name, quantity, desc, shapeId, typeId, seasonId)
+        const journalMoodInput = document.querySelector("#journalMood").value;
+        //updateJournalEntry has 2 paramaters to pass through, 
+        //1st is the entry id and 2nd is our updated journal entry presented as our newJournalEntryObject with the required paramaters using the above variables assigned to the values of the input boxes
+        //(name, quantity, desc, shapeId, typeId, seasonId)
+        //after that updateJournalEntry has been put to the database then we return with an invokation of the getJournalEntries "get request"
+        //then we take that response, clear our input fields, and return the render object with the showJournalEntries Method that is invoked with the response from our get request
 		API.updateJournalEntry(hiddenEntryId.value, newJournalEntryObject(journalDateInput, journalTitleInput, journalEntryInput, journalMoodInput))
         .then(() => {
-            return API.getJournalEntries(newJournalEntryObject)
+            return API.getJournalEntries()
         })
         .then((allObjectsFromAPI) => {
             clearInputs()   
@@ -86,7 +90,10 @@ saveJournalEditsButton.addEventListener("click", event => {
             return Render.showJournalEntries(allObjectsFromAPI)
         })   
        // console.log(journalEntrySubmit)
-		
+        //IF it is TRUE, there is NO entry ID...
+        // NEW Journal Entry, we have a nested If else statement,
+            // IF  all fields are not populated give an alert 
+            // ELSE set a varaible equal to the our newjournalobject with the paramaters in the corresponding location for the arguments (properties) 
     } else {
         const journalDate = document.getElementById("journalDate").value
         const journalTitle = document.getElementById("journalTitle").value
@@ -106,7 +113,10 @@ saveJournalEditsButton.addEventListener("click", event => {
        else {
            // variable that is equal to the response of a function invoked with the paramaters captured from the form
             const journalEntrySubmit = newJournalEntryObject(journalDate, journalTitle, journalEntry, journalMood )
-            
+            //pass the new variable through the save "POST" fetch request 
+            //.then return with a get request
+            //.then take that response, clear the input fields, 
+            //....and return the render object using the showJournalEntries method that is invoked with the getJournalEntries Response
             API.saveJournalEntry(journalEntrySubmit).then(() => {
                 return API.getJournalEntries()
             })
@@ -118,5 +128,13 @@ saveJournalEditsButton.addEventListener("click", event => {
         }
         
             
+    }
+})
+
+// JOURNAL ENTRY FILTER BASED ON MOOD SELECTED IN THE MOOD FILTERS radio button form
+const radioButton = document.getElementsByName('mood')
+radioButton.forEach(button => {
+    if(button.value = journalMood) {
+        
     }
 })
